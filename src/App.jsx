1b36df35e9cf9db8b5a6d05781a3d62bf37b6655
Vanitaa Rai <vanitrai@gmail.com>
2024-04-23 14:25:37 +0800

    feat sucessfull fetch from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import "./App.css";
+
+async function logRecipes() {
+  try {
+    const res = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+        import.meta.env.VITE_SPOON_API_KEY
+      }`
+    );
+
+    if (!res.ok) {
+      throw new Error("Network response was not OK");
+    }
+
+    const recipes = await res.json();
+    console.log(recipes);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
 
 function App() {
-
+  logRecipes();
 
   return (
     <>
-      <h1>WanderChef</h1>
+      <h1>WonderChef</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
