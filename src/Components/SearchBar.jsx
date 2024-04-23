055bea79727d0143de9f2b849a35b7db79924755
Vanitaa Rai <vanitrai@gmail.com>
2024-04-23 21:10:59 +0800

    feat search bar setup

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,1 +1,71 @@
-export default function SearchBar() {}
+import { useState } from "react";
+import { Form } from "react-bulma-components";
+
+const { Input, Field, Control, Label } = Form;
+
+export default function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+  const [mealType, setMealType] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [diet, setDiet] = useState("");
+  const [intolerance, setIntolerance] = useState("");
+
+  //use function passed in prop
+  const handleSearch = () => {
+    const searchParams = {
+      query,
+      mealType,
+      cuisine,
+      diet,
+      intolerance,
+    };
+    onSearch(searchParams);
+  };
+  console.log(query);
+  return (
+    <>
+      <br />
+      <Field align="centre" kind="addons">
+        <Control>
+          <Input
+            className="input is-rounded"
+            type="text"
+            placeholder="Search recipes"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Control>
+        <Control>
+          <button className="button is-dark is-rounded" onClick={handleSearch}>
+            Search
+          </button>
+        </Control>
+      </Field>
+      <Field align="center" kind="group">
+        <select value={mealType} onChange={(e) => setMealType(e.target.value)}>
+          <option value="">Meal Type</option>
+          <option value="main course">Main Course</option>
+          <option value="dessert">Dessert</option>
+        </select>
+        <select value={cuisine} onChange={(e) => setCuisine(e.target.value)}>
+          <option value="">Select Cuisine</option>
+          <option value="italian">Italian</option>
+          <option value="mexican">Mexican</option>
+        </select>
+        <select value={diet} onChange={(e) => setDiet(e.target.value)}>
+          <option value="">Select Diet</option>
+          <option value="vegan">Vegan</option>
+          <option value="vegetarian">Vegetarian</option>
+        </select>
+        <select
+          value={intolerance}
+          onChange={(e) => setIntolerance(e.target.value)}
+        >
+          <option value="">Select Intolerance</option>
+          <option value="dairy">Dairy</option>
+          <option value="gluten">Gluten</option>
+        </select>
+      </Field>
+    </>
+  );
+}
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,15 +3,34 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import NavBar from "../Components/NavBar";
 import SearchBar from "../Components/SearchBar";
+import { useState } from "react";
 
 export default function LandingPage() {
-  async function handleSearch(searchParams) {}
+  const [recipes, setRecipes] = useState([]);
+
+  async function handleSearch(searchParams) {
+    const { query, mealType, cuisine, diet, intolerance } = searchParams;
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+      import.meta.env.VITE_SPOON_API_KEY
+    }&query=${query}&type=${mealType}&cuisine=${cuisine}&diet=${diet}&intolerance=${intolerance}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const fetchedRecipes = await response.json();
+      console.log(fetchedRecipes); // check data received
+    } catch (error) {
+      console.error("There was a problem with your fetch operation:", error);
+    }
+  }
 
   return (
     <>
       <NavBar />
       <Header />
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
       <Footer />
     </>
   );
