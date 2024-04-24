07fe648692046479ed535d075d02660830495157
Vanitaa Rai <vanitrai@gmail.com>
2024-04-24 16:46:37 +0800

    feat recipe details - instructions can appear

--- src/Pages/RecipeDetailsPage.jsx
+++ src/Pages/RecipeDetailsPage.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
+import { Block, Panel } from "react-bulma-components";
 import { useParams } from "react-router-dom";
+import NavBar from "../Components/NavBar";
+import Header from "../Components/Header";
+
 //fetch data from recipeInfo url using id param
 export default function RecipeDetailsPage() {
   const { id } = useParams();
   const apiUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${
     import.meta.env.VITE_SPOON_API_KEY
   }&addWinePairing=false&addTasteData=false`;
-  const [recipeDetails, setRecipeDetails] = useState(null); //initialize as null
+  const [recipeDetails, setRecipeDetails] = useState({}); //initialize as empty obj (not null)
   //useEffect(() => {},[id]) to fetch data based on id -->dependency array
   useEffect(() => {
     async function fetchRecipeDetails() {
@@ -18,9 +22,6 @@ export default function RecipeDetailsPage() {
         const data = await response.json();
         console.log(data); // check data received //is {id:*, image:*, servings:*, title:*, analyzedInstrctions: [{steps: [{number:1, step: xxx}]}], }
 
-        // const fetchedRecipes = data.results;
-        // console.log(fetchedRecipes); //[{id: xx, title: xx, image: xx}]
-
         setRecipeDetails(data);
       } catch (error) {
         console.error("There was a problem with your fetch operation:", error);
@@ -29,5 +30,35 @@ export default function RecipeDetailsPage() {
     fetchRecipeDetails();
   }, [apiUrl, id]);
 
-  return <>RecipeDetailsPage {id}</>;
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Block />
+      {/* RecipeDetailsPage {id} */}
+      <Panel>
+        <Panel.Header>{recipeDetails.title}</Panel.Header>
+        <Panel.Block>
+          <img size="2by1" src={recipeDetails.image} />
+          {/* <p>details</p> */}
+          <h3>Instructions:</h3>
+          {recipeDetails.analyzedInstructions &&
+          recipeDetails.analyzedInstructions.length > 0 ? (
+            recipeDetails.analyzedInstructions.map((instruction, index) => (
+              <div key={index}>
+                <h4>Step {index + 1}</h4>
+                <ul>
+                  {instruction.steps.map((step, stepIndex) => (
+                    <li key={stepIndex}>{step.step}</li>
+                  ))}
+                </ul>
+              </div>
+            ))
+          ) : (
+            <p>Fetching details</p>
+          )}
+        </Panel.Block>
+      </Panel>
+    </>
+  );
 }
