a3deb4015e7d4ff2735841442f1ea3c6623c2457
Vanitaa Rai <vanitrai@gmail.com>
2024-04-24 23:55:05 +0800

    feat fetching airtable using curl

--- src/AirtableContext.jsx
+++ src/AirtableContext.jsx
@@ -1,38 +1,45 @@
 import { createContext, useEffect, useState } from "react";
-import Airtable from "airtable";
-
-//initialize airtable with my API key and base ID
-const base = new Airtable({
-  apiKey: import.meta.env.VITE_AIRTABLE_API_KEY,
-}).base("appqsD3wc5xZBbGMa");
 
 //creating context for useContext later
 export const AirtableContext = createContext();
 
 //using children prop to access nested components --> wrapped by this Component
 export default function AirtableContextProvider({ children }) {
-  const [records, setRecords] = useState([]); //initialize as empty array ?null
+  const [savedRecipes, setSavedRecipes] = useState([]); //initialize as empty array ?null
+  const apiUrl =
+    "https://api.airtable.com/v0/appqsD3wc5xZBbGMa/MySavedRecipes?view=Grid%20view";
 
-  //fetch records (copied select...) from airtable when component mounts --> empty dependency array //useEffect(() => {}, [])
+  //fetch records from airtable when component mounts --> empty dependency array //useEffect(() => {}, [])
   useEffect(() => {
-    base("MySavedRecipes")
-      .select({
-        view: "Grid view",
-      })
-      .firstPage(function (err, records) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        setRecords(records); //assign to state
-        records.forEach(function (record) {
-          console.log("Retrieved", record.get("MyRecipeID"));
+    async function fetchSavedRecipes() {
+      try {
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_KEY}`,
+          },
         });
-      });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        console.log(data.records);
+        setSavedRecipes(data.records); // airtable returns obj--> {records: [{fields: {header: data}}]}
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchSavedRecipes();
   }, []);
 
+  //need to then display in children component--> use map to iterate thru airtable data
+
   return (
-    <AirtableContext.Provider value={{ records }}>
+    <AirtableContext.Provider value={{ savedRecipes }}>
       {children}
     </AirtableContext.Provider>
   );
