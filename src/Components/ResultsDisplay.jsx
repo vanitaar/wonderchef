b6a77d49606d26996373d96fb5c422af8926d1b4
Vanitaa Rai <vanitrai@gmail.com>
2024-04-24 12:02:05 +0800

    feat resultsDisplay if no results

--- src/Components/ResultsDisplay.jsx
+++ src/Components/ResultsDisplay.jsx
@@ -1,14 +1,21 @@
 import RecipeCard from "./RecipeCard";
-import { Container, Columns } from "react-bulma-components";
+import { Container, Columns, Heading } from "react-bulma-components";
 
 export default function ResultsDisplay({ recipes }) {
   return (
     <Container>
-      <Columns multiline={true}>
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
-      </Columns>
+      {/* if no recipe found */}
+      {recipes.length >= 1 ? (
+        <Heading subtitle>
+          Looks like we &apos;re fresh out of recipes here!
+        </Heading>
+      ) : (
+        <Columns multiline={true}>
+          {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          ))}
+        </Columns>
+      )}
     </Container>
   );
 }
