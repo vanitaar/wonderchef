7853a16ff59050eda90a9de8b8ffb421ba9553ff
Vanitaa Rai <vanitrai@gmail.com>
2024-04-26 12:09:27 +0800

    feat resultsDisplay- replaced ternary operator style--> if statement before return ->better readabilty

--- src/Components/ResultsDisplay.jsx
+++ src/Components/ResultsDisplay.jsx
@@ -2,20 +2,24 @@ import RecipeCard from "./RecipeCard";
 import { Container, Columns, Heading } from "react-bulma-components";
 
 export default function ResultsDisplay({ recipes }) {
+  {
+    /* if no recipe found */
+  }
+  if (recipes.length === 0) {
+    return (
+      <Heading subtitle>
+        Looks like we &apos;re fresh out of recipes here!
+      </Heading>
+    );
+  }
+
   return (
     <Container>
-      {/* if no recipe found */}
-      {recipes.length !== 0 ? (
-        <Columns multiline={true}>
-          {recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} />
-          ))}
-        </Columns>
-      ) : (
-        <Heading subtitle>
-          Looks like we &apos;re fresh out of recipes here!
-        </Heading>
-      )}
+      <Columns multiline={true}>
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </Columns>
     </Container>
   );
 }
