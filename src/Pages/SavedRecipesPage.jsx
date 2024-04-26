ea651addec7e0b45caf966bc3d51374f0d841c5c
Vanitaa Rai <vanitrai@gmail.com>
2024-04-26 10:35:13 +0800

    feat SavedRecipePage - remove old comments; added delete button to implement

--- src/Pages/SavedRecipesPage.jsx
+++ src/Pages/SavedRecipesPage.jsx
@@ -1,26 +1,16 @@
 import { useContext } from "react";
 import { AirtableContext } from "../AirtableContext";
-import { Container, Content, Heading, Section } from "react-bulma-components";
+import {
+  Container,
+  Content,
+  Heading,
+  Section,
+  Button,
+} from "react-bulma-components";
 import { Link } from "react-router-dom";
 
 export default function SavedRecipesPage() {
   const { savedRecipes } = useContext(AirtableContext);
-  //need to then display in this children component--> use map to iterate thru airtable data
-
-  //   const navigate = useNavigate();
-
-  //   //using Callback to prevent onClick to be called on rendering
-  //   const navigateToDetails = useCallback(
-  //     (id) => {
-  //       navigate(`/recipe/${id}`);
-  //     },
-  //     [navigate]
-  //   );
-
-  //   function navigateToDetails(id) {
-  //     // navigate(`/recipe/${id}`); -->error
-  //     window.location.href = `/recipe/${id}`;
-  //   }
 
   return (
     <Container>
@@ -31,9 +21,8 @@ export default function SavedRecipesPage() {
             <Section key={index}>
               <h5>{recipe.fields.TItle}</h5>
               <img src={recipe.fields.ImgSrc} alt={recipe.fields.TItle} />
-              {/* <Button onClick={navigateToDetails(recipe.fields.apiID)}>
-                See recipe details
-              </Button> */}
+              {/* delete button yet to work*/}
+              <Button remove />
               <Link to={`/recipe/${recipe.fields.apiID}`}>
                 See Recipe Details
               </Link>
