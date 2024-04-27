378b250f1a62021b1b9136ce19d421c692908530
Vanitaa Rai <vanitrai@gmail.com>
2024-04-28 03:53:24 +0800

    header w logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,21 @@
+import { Block, Media } from "react-bulma-components";
+import logo from "./asset/logo.png";
+
 export default function Header() {
   return (
     <section className="hero is-warning is-halfheight">
       <div className="hero-body">
         <div className="">
-          <p className="title">WonderChef</p>
-          <p className="subtitle">Explore and Savour</p>
+          <Media>
+            <Media.Item align="left">
+              <img src={logo} alt="Logo" width={200} height={200} />
+            </Media.Item>
+            <Media.Item align="right">
+              <Block />
+              <p className="title">WonderChef</p>
+              <p className="subtitle">Explore and Savour</p>
+            </Media.Item>
+          </Media>
         </div>
       </div>
     </section>
