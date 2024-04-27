3efee58747c18d82e2e3e046cd6944457e4d3b75
Vanitaa Rai <vanitrai@gmail.com>
2024-04-27 15:09:46 +0800

    feat favicon, logo, trying to do up navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,41 @@
 // import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar } from "react-bulma-components";
 
 export default function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/quick-recipes">Quick&Random Recipes</Link>
-        </li>
-        <li>
-          <Link to="/substitute-ingredients">
-            Search Substitute Ingredients
-          </Link>
-        </li>
-        <li>
-          <Link to="/saved-recipes">Saved Recipes</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <Navbar>
+        <Navbar.Brand>
+          <Navbar.Item>
+            <Link to="/">
+              <img src="../src/assets/logo.png" alt="WonderChef Logo" />
+              <span>WonderChef</span>
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item renderAs="div">
+            <Link to="/saved-recipes" className={"navbar-item"}>
+              Saved Recipes
+            </Link>
+          </Navbar.Item>
+          <Navbar.Burger />
+        </Navbar.Brand>
+      </Navbar>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/quick-recipes">Quick&Random Recipes</Link>
+          </li>
+          <li>
+            <Link to="/substitute-ingredients">
+              Search Substitute Ingredients
+            </Link>
+          </li>
+          <li>
+            <Link to="/saved-recipes">Saved Recipes</Link>
+          </li>
+        </ul>
+      </nav> */}
+    </>
   );
 }
